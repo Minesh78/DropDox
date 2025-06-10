676513bc397a6c424d1f5a1e212e00f552a9b766
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2025-06-10 23:29:56 +0530

    added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,36 @@
 import type { Metadata } from "next";
-import { Inter,Open_Sans } from "next/font/google";
+import { Inter, Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Providers } from "./providers";
 import "../styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const openSans = Open_Sans({
-  subsets: ["latin"],
-  display: "swap",
+	subsets: ["latin"],
+	display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "DropDox",
-  description: "Secure cloud storage for your images, powered by ImageKit",
+	title: "DropDox",
+	description: "Secure cloud storage for your images, powered by ImageKit",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className="dark">
-        <body
-          className={`$openSans.className } antialiased bg-background text-foreground`}
-        >
-          <Providers>{children}</Providers>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en" className="dark">
+				<body
+					className={`$openSans.className } antialiased bg-background text-foreground`}>
+					<Providers>
+						{children}
+						<SpeedInsights />
+					</Providers>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -4,15 +4,14 @@ import Link from "next/link";
 import Navbar from "@/components/Navbar";
 
 export default function SignUpPage() {
-  return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-      {/* Use the unified Navbar component */}
-      <Navbar />
+	return (
+		<div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+			{/* Use the unified Navbar component */}
+			<Navbar />
 
-      <main className="flex-1 flex justify-center items-center p-6">
-        <SignUpForm />
-      </main>
-
-    </div>
-  );
+			<main className="flex-1 flex justify-center items-center p-6 w-full">
+				<SignUpForm />
+			</main>
+		</div>
+	);
 }
